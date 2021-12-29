40a73832c6f4048e47920f2f5151fc29dcefd065
Abdullah Ridwan <abdullahridwan73@icloud.com>
2021-12-29 00:37:38 -0500

    Initial Commit

---
+++ drugInfo/drugInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  drugInfoApp.swift
+//  drugInfo
+//
+//  Created by Abdullah Ridwan on 12/29/21.
+//
+
+import SwiftUI
+
+@main
+struct drugInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
